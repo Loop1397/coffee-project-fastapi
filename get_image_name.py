fefe67af3d3366688ee516f8bbc83384c7492f5a
Loop1397 <shwlsdn0906@gmail.com>
2024-03-17 23:08:52 +0900

    request 요청을 통해 url로 썸네일 다운받기

--- get_image_name.py
+++ get_image_name.py
@@ -1,6 +1,10 @@
 import json
 import sys
 import re
+import urllib.request
+import time
+
+
 
 def extract_filename(url):
     # URL에서 파일 이름 추출을 위한 정규표현식
@@ -23,4 +27,18 @@ with open('products.json', 'r') as f:
 json_data = list(json_data)
 
 for data in json_data:
-    print(extract_filename(data['thumbnailUrl']))
+    # 파일 이름
+    file_name = extract_filename(data['thumbnailUrl'])
+    # 파일 url
+    url = data['thumbnailUrl']
+    
+    # time check
+    start = time.time()
+
+    # 이미지 요청 및 다운로드
+    urllib.request.urlretrieve(url, file_name)
+
+    # 이미지 다운로드 시간 체크
+    print(time.time() - start)
+    
+    break
