569786ed6430c2fcd0ac50b594117fe0ad11a0e5
Loop1397 <shwlsdn0906@gmail.com>
2024-03-18 22:11:03 +0900

    url에서 이름 가져오는 파일 import 대응하게 변경

--- get_image_name.py
+++ get_image_name.py
@@ -4,8 +4,6 @@ import re
 import urllib.request
 import time
 
-
-
 def extract_filename(url):
     # URL에서 파일 이름 추출을 위한 정규표현식
     pattern = r'[^/]+\.jpg|[^/]+\.png|[^/]+\.jpeg'
@@ -20,25 +18,28 @@ def extract_filename(url):
         return None
 
 sys.stdout.reconfigure(encoding='utf-8')
-with open('products.json', 'r') as f:
 
-    json_data = json.load(f)
+# with open('products.json', 'r') as f:
+#     json_data = json.load(f)
 
-json_data = list(json_data)
+# json_data = list(json_data)
 
-for data in json_data:
-    # 파일 이름
-    file_name = extract_filename(data['thumbnailUrl'])
-    # 파일 url
-    url = data['thumbnailUrl']
+def getThumbnailNameFromUrl(productsData):
+    for data in productsData:
+        # 파일 이름
+        file_name = extract_filename(data['thumbnailUrl'])
+        # 파일 url
+        url = data['thumbnailUrl']
+        
+        # time check
+        start = time.time()
+
+        # 이미지 요청 및 다운로드
+        urllib.request.urlretrieve(url, file_name)
+
+        # 이미지 다운로드 시간 체크
+        print(time.time() - start)
+        
+        break
     
-    # time check
-    start = time.time()
-
-    # 이미지 요청 및 다운로드
-    urllib.request.urlretrieve(url, file_name)
-
-    # 이미지 다운로드 시간 체크
-    print(time.time() - start)
-    
-    break
+    return productsData
