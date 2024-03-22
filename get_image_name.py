cde725a90f48c3aded1f01db2a36048fe144d7d9
Loop1397 <shwlsdn0906@gmail.com>
2024-03-22 23:11:59 +0900

    파일 저장 장소 변경

--- get_image_name.py
+++ get_image_name.py
@@ -1,9 +1,11 @@
 import json
 import sys
 import re
-import urllib.request
+import requests
 import time
+import os
 
+# url에서 파일 이름 가져오기
 def extract_filename(url):
     # URL에서 파일 이름 추출을 위한 정규표현식
     pattern = r'[^/]+\.jpg|[^/]+\.png|[^/]+\.jpeg'
@@ -19,27 +21,26 @@ def extract_filename(url):
 
 sys.stdout.reconfigure(encoding='utf-8')
 
-# with open('products.json', 'r') as f:
-#     json_data = json.load(f)
 
-# json_data = list(json_data)
-
-def getThumbnailNameFromUrl(productsData):
+def getThumbnailFromUrl(productsData):
     for data in productsData:
         # 파일 이름
-        file_name = extract_filename(data['thumbnailUrl'])
+        fileName = extract_filename(data['thumbnailUrl'])
         # 파일 url
         url = data['thumbnailUrl']
-        
-        # time check
-        start = time.time()
 
         # 이미지 요청 및 다운로드
-        urllib.request.urlretrieve(url, file_name)
-
-        # 이미지 다운로드 시간 체크
-        print(time.time() - start)
+        response = requests.get(url)
+        with open(os.path.join('public/img/', fileName), 'wb') as file:
+            file.write(response.content)
         
         break
     
     return productsData
+
+with open('products.json', 'r') as f:
+    json_data = json.load(f)
+
+json_data = list(json_data)
+
+getThumbnailFromUrl(json_data)
--- image_resize.py
+++ image_resize.py
@@ -1,8 +1,11 @@
 from PIL import Image
+import time
+import os
 
-input_img = Image.open("103178096062351735_1015449818.jpg")
-print("input image size:", input_img.size)
+def imageResizing(fileName):
+    imgPath = os.path.join('public/img', fileName)
+    inputImg = Image.open(imgPath)
 
-output_img = input_img.resize((700, 700))
-print("output image size:", output_img.size)
-output_img.save("103178096062351735_1015449818New.jpg")
+    # 이미지 700x700으로 변환
+    output_img = inputImg.resize((700, 700))
+    output_img.save(imgPath)
